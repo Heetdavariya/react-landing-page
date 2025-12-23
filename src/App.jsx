fcfe0295ae86a74bbadebb1be3d0e32a9e50fa90
Heet Davariya <heetdavariya2@gmail.com>
2025-12-23 12:09:32 +0530

    Fix case-sensitive import issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Features from "./components/Features"
 import Hero from "./components/Hero"
-import Navbar from "./components/navbar"
+import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 
 export default function App(){
